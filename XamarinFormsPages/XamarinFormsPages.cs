89ddabfb4b7a396bdde5bc41f546f5ebc44c4bf6
Jeffrey Cabrera <jeffcabrera@Jeffreys-MacBook-Air.local>
2016-05-13 09:28:31 +0800

    late upload

---
+++ XamarinFormsPages/XamarinFormsPages.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace XamarinFormsPages
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+
+			//MainPage = new PagesDemo();
+			MainPage = new PagesDemoXaml();
+			//MainPage = new MasterDetailSamp();
+			//NavigationPage.SetHasNavigationBar (MainPage, false);
+			//NavigationPage.SetTitleIcon (MainPage, false);
+			NavigationPage.SetHasBackButton (MainPage, true);
+
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
